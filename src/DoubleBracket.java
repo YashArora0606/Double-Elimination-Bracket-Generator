0e608982a55457a2f54ef3353057b837b87a7b2a
Yash <yash_arora6@hotmail.com>
2018-09-19 11:09:31 -0400

    hard code

--- src/DoubleBracket.java
+++ src/DoubleBracket.java
@@ -4,25 +4,77 @@ public class DoubleBracket extends Bracket{
 
  @Override
  int getNumberOfTeams() {
-  // TODO Auto-generated method stub
-  return 6;
+  // HARD CODED
+  return 3;
  }
 
  @Override
  int getNumberOfRounds() {
-  // TODO Auto-generated method stub
-  return 6;
+  // HARD CODED
+  return 3;
  }
 
  @Override
  int getNumberOfMatchesInRound(int round) {
-  // TODO Auto-generated method stub
+  // HARD CODED
+	 
+	 if (round == 1) {
+		 return 1;
+	 } else if (round == 2) {
+		 return 2;
+	 } else if (round == 3) {
+		 return 1;
+	 }
   return 0;
+  
  }
 
  @Override
  String[][] getTeamsInMatch(int round, int matchNumber) {
-  // TODO Auto-generated method stub
+	 
+  if (round == 1) {
+	  
+	  if (matchNumber == 1) {
+		  String[][] arr = new String[2][1];
+		  arr[0][0] = "A";
+		  arr[1][0] = "B";
+		  return arr;
+	  }
+	  
+  } else if (round == 2) {
+	  
+	  if (matchNumber == 1) {
+		  String[][] arr = new String[2][2];
+		  arr[0][0] = "A";
+		  arr[0][1] = "B";
+		  arr[1][0] = "C";
+		  return arr;
+	  }
+	  
+	  if (matchNumber == 2) {
+		  String[][] arr = new String[2][3];
+		  arr[0][0] = "A";
+		  arr[0][1] = "B";
+		  
+		  arr[1][0] = "A";
+		  arr[1][1] = "B";
+		  arr[1][2] = "C";
+		  return arr;
+	  }
+	  
+  } else if (round == 3) {
+	  if (matchNumber == 1) {
+		  String[][] arr = new String[2][3];
+		  arr[0][0] = "A";
+		  arr[0][1] = "B";
+		  arr[0][2] = "C";
+		  
+		  arr[1][0] = "A";
+		  arr[1][1] = "B";
+		  arr[1][2] = "C";
+		  return arr;
+	  }
+  }
   return null;
  }
 
