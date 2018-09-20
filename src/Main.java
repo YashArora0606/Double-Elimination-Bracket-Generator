04a1ced826a29fccb34c56e8b667bef3115a476d
Yash <yash_arora6@hotmail.com>
2018-09-20 10:35:00 -0400

    some random shit

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,8 @@ public class Main {
 		teams.add('a');
 		teams.add('c');
 		teams.add('e');
+		
 
-		
-//		for (int i = 0; i < teams.size(); i++) {
-//			System.out.print(teams.get(i) + "\t");
-//		}
-		
-		//System.out.println();
 		
 	
 		
@@ -31,6 +26,10 @@ public class Main {
 	}
 	
 	public static void printRound(ArrayList<Character> teams) {
+		
+		if (teams.size()%2 == 1 && teams.size() != 1) {
+			teams.add(' ');
+		}
 		
 		if (teams.size() == 1) {
 			System.out.println(teams.get(0));
@@ -46,7 +45,7 @@ public class Main {
 	
 	public static void getRemainingTeams(ArrayList<Character> teams) {
 		ArrayList<Character> teamsLeft= new ArrayList<Character>();
-		for (int i = 0; i < Math.floor(teams.size()/2); i++) {
+		for (int i = 0; i < Math.ceil((double) (teams.size()/2)); i++) {
 			char winner = fight(teams.get(i*2), teams.get((i*2)+1));
 			teamsLeft.add(winner);
 		}
@@ -54,6 +53,14 @@ public class Main {
 	}
 	
 	public static char fight(char team1, char team2) {
+		
+		if (team1 == ' ') {
+			return team2;
+		} else  if (team2 == ' ') {
+			return team1;
+		}
+		
+		
 		if (team1 < team2) {
 			return team1;
 		} else {
