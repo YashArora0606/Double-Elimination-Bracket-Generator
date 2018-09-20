845a27a9da62bd4092ae13f2f0e2fae45890fe3a
Yash <yash_arora6@hotmail.com>
2018-09-20 10:43:39 -0400

    what

--- src/Main.java
+++ src/Main.java
@@ -2,63 +2,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-<<<<<<< HEAD
- public static void main(String[] args) {
-  
-  
-  ArrayList<Character> teams = new ArrayList<Character>();
-
-  teams.add('d');
-  teams.add('b');
-  teams.add('a');
-  teams.add('c');
-  teams.add('e');
-
-
-  
-  printRound(teams);
-
-
- 
-  
-  
- }
- 
- public static void printRound(ArrayList<Character> teams) {
-  
-  if (teams.size() == 1) {
-   System.out.println(teams.get(0));
-   
-  } else {
-   for (int i = 0; i < teams.size(); i++) {
-    System.out.print(teams.get(i) + "\t");
-   }
-   System.out.println();
-   getRemainingTeams(teams);
-  }
- }
- 
- public static void getRemainingTeams(ArrayList<Character> teams) {
-  ArrayList<Character> teamsLeft= new ArrayList<Character>();
-  for (int i = 0; i < Math.floor(teams.size()/2); i++) {
-   char winner = fight(teams.get(i*2), teams.get((i*2)+1));
-   teamsLeft.add(winner);
-  }
-  printRound(teamsLeft);
- }
- 
- public static char fight(char team1, char team2) {
-  if (team1 < team2) {
-   return team1;
-  } else {
-   return team2;
-  }
-  
- 
- }
-
- 
-=======
 	public static void main(String[] args) {
 		
 		
@@ -138,7 +81,6 @@ public class Main {
 	}
 
 	
->>>>>>> e212ca6133432784090adbd9e4a36920fbd09d87
 }
 
 
