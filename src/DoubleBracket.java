c386896b791f5b70227108234f6be309baf1afd7
Yash <yash_arora6@hotmail.com>
2018-09-25 11:28:19 -0400

    boi

--- src/DoubleBracket.java
+++ src/DoubleBracket.java
@@ -3,28 +3,48 @@ import java.util.ArrayList;
 public class DoubleBracket extends Bracket{
 
 	ArrayList<Team> teams;
+	int numRounds;
+	int numTeams;
+	int numMatches;
+	int[] numMatchesInRound;
   
 	// DONE
 	DoubleBracket(ArrayList<Team> teams) {
 		this.teams = teams;
+		
+		numRounds = (int)(Math.ceil(Math.log(teams.size())/Math.log(2)));
+		numTeams = teams.size();
+		numMatches = (teams.size() - 1) * 2;
+		
+		
+		
+		int L2 = log(teams.size(), 2);
+		int surplus = (int)(Math.ceil(L2) + Math.ceil(log(L2, 2)));
+		
+		
+		int total = (int)(Math.ceil(Math.log(teams.size())/Math.log(2))) + surplus;
+		
 	}
 	// DONE
 	@Override
 	int getNumberOfTeams() {
-		return teams.size();
+		return numTeams;
 	}
 	// DONE
 	@Override
 	int getNumberOfRounds() {
-		return (int)(Math.ceil(Math.log(teams.size())/Math.log(2)));
+		return numRounds;
 	}
+	
 
 
- 
+	static int log(int x, int base) {
+	    return (int) (Math.log(x) / Math.log(base));
+	}
  
  @Override
  int getNumberOfMatchesInRound(int round) {
-  return 0;  
+  return 5;  
  }
 
  @Override
